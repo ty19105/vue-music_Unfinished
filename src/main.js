68163d98f4b64484a81b7509a9a95cd17b2efeef
wq93 <214180117@qq.com>
2017-07-06 09:53:16 +0800

    添加stylus依赖

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-
-Vue.config.productionTip = false
+// import router from './router'
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   render: h => h(App)
 })
