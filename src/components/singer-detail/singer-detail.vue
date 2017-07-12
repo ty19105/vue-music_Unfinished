b647a6be511c3bbc8e6154f7f62bfca9b35f34d8
wq93 <214180117@qq.com>
2017-07-12 18:42:08 +0800

    singer-detail和singer页面的过渡

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="singer-detail">
-    我是歌手详情
-  </div>
+  <transition name="slide">
+    <div class="singer-detail">
+      我是歌手详情
+    </div>
+  </transition>
 </template>
 
 <script type="text/ecmascript-6">
@@ -18,4 +20,10 @@
     right: 0
     bottom: 0
     background: $color-background
+
+  .slide-enter-active, .slide-leave-active
+    transition: all 0.5s
+
+  .slide-enter, .slide-leave-to
+    transform: translate3d(100%, 0, 0)
 </style>
