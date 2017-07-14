f5afde279cee0a14af9d31d8b7b0af925816bc30
wq93 <214180117@qq.com>
2017-07-14 15:15:46 +0800

    music-list组件完善 loading效果

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="music-list">
     <div class="back">
-      <i class="icon-back"></i>
+      <i class="icon-back" @click='back'></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div class="play" v-show="songs.length>0" ref="playBtn">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
@@ -13,6 +19,9 @@
       <div class="song-list-wrapper">
         <songlist :songs="songs" :rank="rank"></songlist>
       </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
@@ -20,6 +29,7 @@
 <script type="text/ecmascript-6">
   import Scroll from '../../base/scroll/scroll.vue'
   import Songlist from '../../base/song-list/song-list.vue'
+  import Loading from '../../base/loading/loading.vue'
   import {prefixStyle} from '../../common/js/dom'
 
   const RESERVED_HEIGHT = 40
@@ -68,6 +78,9 @@
     methods: {
       scroll(pos) {
         this.scrollY = pos.y
+      },
+      back() {
+        this.$router.back() // 回退到上一级
       }
     },
     watch: {
@@ -91,9 +104,11 @@
           zIndex = 10
           this.$refs.bgImage.style.paddingTop = 0
           this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+          this.$refs.playBtn.style.display = 'none' // 隐藏随机按钮
         } else {
           this.$refs.bgImage.style.paddingTop = '70%'
           this.$refs.bgImage.style.height = 0
+          this.$refs.playBtn.style.display = 'block'
         }
 
         this.$refs.bgImage.style.zIndex = zIndex
@@ -102,7 +117,8 @@
     },
     components: {
       Scroll,
-      Songlist
+      Songlist,
+      Loading
     }
   }
 </script>
