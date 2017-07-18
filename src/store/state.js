0ec6677b38728a6b26f07cd6286c544d6aa4f48d
wq93 <214180117@qq.com>
2017-07-18 22:59:24 +0800

    配置播放器的vuex的参数set和get

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,12 @@
+import {playMode} from '../common/js/config'
 const state = {
-  singer: {}
+  singer: {}, //歌手
+  playing: false, // 播放状态
+  fullScreen: false, // 全屏
+  playList: [], // 播放列表
+  sequenceList: [], // 排序列表
+  mode: playMode.sequence, //播放模式
+  currentIndex: -1 // 当前播放的歌曲索引
 }
 
 export default state
